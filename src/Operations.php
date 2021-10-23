e0eea8c5b7c1a270285ad3fbacf6097ef0588191
Svyatoslav <pyrokiness@mail.ru>
2021-10-23 20:11:23 +0300

    Add times operation

--- src/Operations.php
+++ src/Operations.php
@@ -14,4 +14,12 @@ class Operations
             $num1->getIm() + $num2->getIm()
         );
     }
+
+    public static function times(Complex $num1, Complex $num2)
+    {
+        $re_part = $num1->getRe() * $num2->getRe() - $num1->getIm() * $num2->getIm();
+        $im_part = $num1->getRe() * $num2->getIm() + $num1->getIm() * $num2->getRe();
+
+        return new Complex($re_part, $im_part);
+    }
 }
